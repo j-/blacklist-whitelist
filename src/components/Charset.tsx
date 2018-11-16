5ba64fe9d4bb8fd8b12584a90972f6abf585a9cf
j- <j@skeoh.com>
2018-11-16 19:41:53 +1100

    Introduce charset row component

--- src/components/Charset.tsx
+++ src/components/Charset.tsx
@@ -5,22 +5,28 @@ import './Charset.css';
 const CHAR_ROWS = 0x8;
 const CHAR_COLS = 0x10;
 
+const CharsetRow: React.StatelessComponent<{ offset: number }> = ({ offset }) => {
+	const cols: React.ReactChild[] = [];
+	for (let x = 0; x < CHAR_COLS; x++) {
+		const charCode = offset + x;
+		cols.push(
+			<div className="Charset-cell" key={charCode}>
+				<Char charCode={charCode} />
+			</div>
+		);
+	}
+	return (
+		<div className="Charset-row">
+			{cols}
+		</div>
+	);
+};
+
 const Charset: React.StatelessComponent = () => {
 	const rows: React.ReactChild[] = [];
 	for (let y = 0; y < CHAR_ROWS; y++) {
-		const cols: React.ReactChild[] = [];
-		for (let x = 0; x < CHAR_COLS; x++) {
-			const charCode = y * CHAR_COLS + x;
-			cols.push(
-				<div className="Charset-cell" key={charCode}>
-					<Char charCode={charCode} />
-				</div>
-			);
-		}
 		rows.push(
-			<div className="Charset-row" key={y}>
-				{cols}
-			</div>
+			<CharsetRow offset={y * CHAR_COLS} key={y} />
 		);
 	}
 	return (
